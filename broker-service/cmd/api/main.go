d76cfe5e26bc58e6eeb7d30728591132518d2f54
Boris <krasnov@nethouse.ru>
2025-11-05 16:54:13 +0300

    создание первичного брокер сервиса

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "8080"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting server on port " + webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
